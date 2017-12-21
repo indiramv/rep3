6055afcd175262be043317e9aa474dd22ca3faeb
indira <indira088@gmail.com>
2017-12-21 13:28:03 -0500

    commot check

--- TngTest/src/test/java/TngJut/TngTest/GroupsDemo.java
+++
@@ -1,5 +0,0 @@
-package TngJut.TngTest;
-
-public class GroupsDemo {
-
-}
--- TngTest/src/test/java/TngJut/TngTest/NewTest.java
+++
@@ -1,9 +0,0 @@
-package TngJut.TngTest;
-
-import org.testng.annotations.Test;
-
-public class NewTest {
-  @Test
-  public void f() {
-  }
-}
